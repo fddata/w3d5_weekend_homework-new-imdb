343034ca9c544625c96501ad7caeb99ca21274e1
fddata <fraserjdouglas@gmail.com>
2018-08-04 17:11:19 +0100

    adds CRUD for Film class

--- console.rb
+++ console.rb
@@ -5,20 +5,33 @@ require_relative 'models/films.rb'
 require_relative 'db/sql_runner.rb'
 
 Customer.delete_all()
+Film.delete_all()
 
 customer1 = Customer.new({
   "name" => "Tom",
   "funds" => 50
   })
-
 customer1.save()
 
 customer2 = Customer.new({
   "name" => "Jane",
   "funds" => 100
   })
-
   customer2.save()
+
+
+film1 = Film.new({
+  "title" => 'Jaws',
+  'price' => 10
+  })
+  film1.save()
+
+film2 = Film.new({
+  'title' => 'Star Wars',
+  'price' => 15
+  })
+film2.save()
+
 
 
 ##CONSOLE TESTS HERE
@@ -27,7 +40,13 @@ customer2 = Customer.new({
 #customer1.delete() #works
 #p Customer.all() #works, returns array
 # customer1.name = "blahblah"
+# customer1.funds = 15
 # customer1.update() #works
+# film1.delete #works
+# film1.title = "BLAH"
+# film1.price = 60
+# film1.update() #works
+
 
 binding.pry
 nil
---
+++ models/films.rb
@@ -0,0 +1,44 @@
+class Film
+
+  attr_accessor :title, :price
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @title = options['title']
+    @price = options['price']
+  end
+
+
+  def save()
+    sql = "INSERT into films(title, price) VALUES ($1, $2) RETURNING id"
+    values = [@title, @price]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i()
+  end
+
+  def self.all
+    sql = "SELECT * FROM films"
+    result = SqlRunner.run(sql)
+    return result.map { |film| Film.new(film)  }
+  end
+
+  def update()
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+
+end
